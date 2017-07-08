fe22c34f733afaff1a2012303bdaf79369c74180
liangsheng <13758208400@163.com>
2017-07-08 22:30:37 +0800

    [add] test git and gitHub123

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -2,8 +2,53 @@
  * Created by liangsheng on 2017/7/8.
  */
 public class TestGit {
+    private String testSource;
+    private String test;
+
+    private String test1;
+
+    /**
+     */
+    public TestGit() {
+        this("1");
+
+    }
+
+    /**
+     * @param testSource
+     */
+    public TestGit(String testSource) {
+        this.testSource = testSource;
+    }
+
     public static void main(String[] args) {
+
+
         System.out.println("test git");
 
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public String getTest1() {
+        return test1;
+    }
+
+    public void setTest1(String test1) {
+        this.test1 = test1;
+    }
+
+    public String getTestSource() {
+        return testSource;
+    }
+
+    public void setTestSource(String testSource) {
+        this.testSource = testSource;
     }
 }
